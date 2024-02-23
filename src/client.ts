ec1d85ef583e39f56d5d8fb605adfd183f767f9f
Rostyslav <rtrukhym@go7.io>
2024-02-23 11:46:06 +0100

    update token

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import {Octokit} from '@octokit/core';
 
 const octokit = new Octokit({
-  auth: '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',
+  auth: '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',
 });
 
 export default octokit;
